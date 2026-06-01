6ff443de2d5758ae421af93187655d296cbf008d
JurgisSakalauskas <jurgutis666@gmail.com>
2026-06-01 20:32:00 +0300

    server action, submit button added

--- app/(dashboard)/tickets/actions.js
+++ app/(dashboard)/tickets/actions.js
@@ -1,5 +1,23 @@
 "use server";
 
-export async function addTicket() {
-  console.log("Hello from the server");
+import { createClient } from "@/utils/supabase/server";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+export async function addTicket(formData) {
+  const ticket = Object.fromEntries(formData);
+
+  const supabase = createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  const { error } = await supabase.from("Tickets").insert({
+    ...ticket,
+    user_email: user.email,
+  });
+
+  revalidatePath("/tickets");
+  redirect("/tickets");
 }
